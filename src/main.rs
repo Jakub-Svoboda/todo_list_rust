4c36a4910fc2895a324777121ea86b4d049b3a0a
Jakub Svoboda <jakub.svoboda.developer@gmail.com>
2024-04-23 14:08:27 +0200

    Serialize and deserialize tickets to/from disk

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use serde::{Serialize, Deserialize};
+use std::fs::File;
+use std::io::Write;
+use std::io::Read;
+
+
+#[derive(Serialize, Deserialize)]
 struct Ticket {
     text: String
 }
@@ -20,6 +27,7 @@ impl Ticket {
     }
 }
 
+#[derive(Serialize, Deserialize)]
 struct Application {
     tickets: Vec<Ticket>,
 }
@@ -40,6 +48,25 @@ impl Application {
             ticket.display();
         }
     }
+
+    fn save_to_file(&self, filename: &str) -> std::io::Result<()> {
+        let serialized = serde_json::to_string(&self.tickets)?;
+
+        let mut file = File::create(filename)?;
+        file.write_all(serialized.as_bytes())?;
+
+        Ok(())
+    }
+
+    fn load_from_file(filename: &str) -> std::io::Result<Self> {
+        let mut file = File::open(filename)?;
+        let mut contents = String::new();
+        file.read_to_string(&mut contents)?;
+
+        let tickets: Vec<Ticket> = serde_json::from_str(&contents)?;
+
+        Ok(Self { tickets })
+    }
 }
 
 fn main() {
@@ -50,4 +77,17 @@ fn main() {
     app.add_ticket(my_ticket);
     app.add_ticket(my_ticket_2);
     app.display();
+
+    if let Err(e) = app.save_to_file("tickets.json") {
+        eprintln!("Failed to save tickets: {}", e);
+    }
+
+    let loaded_app = Application::load_from_file("tickets.json").unwrap();
+    loaded_app.display();
+    let my_ticket_3 = Ticket::new("My third ticket".to_string());
+
+    if let Err(e) = app.save_to_file("tickets.json") {
+        eprintln!("Failed to save tickets: {}", e);
+    }
+
 }
